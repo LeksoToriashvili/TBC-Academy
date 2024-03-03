2fe425f9c7a0887c427fbbebfbab4ebcfde4f0de
LeksoToriashvili <ltoriashvili@gmail.com>
2024-03-03 14:01:07 +0400

    delete homework-1 from main branch

--- homework-1/task-1.py
+++
@@ -1,5 +0,0 @@
-name=input("Enter FirstName: ")
-lastname=input("Enter LastName: ")
-age=int(input("Enter your age: "))
-
-print("\nHello ", name, " ", lastname, ", you are ",  age, " years old.", sep="")
--- homework-1/task-2.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-name=input("Enter your FirstName: ")
-birth_year=int(input("Enter your birth year: "))
-
-current_year=datetime.date.today().year
-age=current_year-birth_year
-print("\nHello ", name, ", you are ", age, " years old.", sep="")
